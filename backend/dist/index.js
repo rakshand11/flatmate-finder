a88010d25525b118642b963922f3dc3c19b06f72
rakshand <rakshand.chhikara.49@gmail.com>
2026-04-04 01:35:01 +0530

    fixed localhost

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -23,7 +23,7 @@ const connectToDatabase = async () => {
 };
 connectToDatabase();
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5174"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
